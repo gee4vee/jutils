c187d1e150b8d889261511de6641e2614d83395b
Gabriel Valencia <gee4vee@me.com>
2017-09-20 20:38:50 -0700

    Move to commons-lang3. Add random string generator.

--- jutils/src/main/java/com/valencia/jutils/jvm/AppForker.java
+++ jutils/src/main/java/com/valencia/jutils/jvm/AppForker.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
